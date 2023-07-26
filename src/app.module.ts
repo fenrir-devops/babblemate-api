f3d07fe3698a51f27a087d293115f1b57a6c3743
fenrir <salzte0506@gmail.com>
2023-07-27 08:56:38 +0900

    fix:add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://gozjxhs:gozjxhs@cluster0.ccmfm.mongodb.net/babblemate?retryWrites=true&w=majority')],
 })
 export class AppModule {}
